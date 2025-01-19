5685cb25dc32b3fd99858a92f2104a2218a2770a
feexie <feezynharmony@gmail.com>
2025-01-19 14:36:45 +0100

    Revert "this commit is created to showcase the use of version control on git revert, after this commit i will be reverting to the initial App.js set up code that react comes with"
    This reverts commit faee92dc499b366248e20d9349a94048801d6f0b.

--- week-2/src/App.js
+++ week-2/src/App.js
@@ -1,29 +1,23 @@
-import React, { useState } from "react";
-import TaskList from "./components/TaskList";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  // Add a new task
-  const addTask = () => {
-    const newTask = {
-      id: tasks.length + 1,
-      title: `Task ${tasks.length + 1}`,
-      description: "This is a new task.",
-    };
-    setTasks([...tasks, newTask]);
-  };
-
-  // Remove a task
-  const removeTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Interactive Task Manager</h1>
-      <button onClick={addTask}>Add Task</button>
-      <TaskList tasks={tasks} removeTask={removeTask} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
